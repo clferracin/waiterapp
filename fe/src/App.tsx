ac70b845d12e076c8bd2ae294b075e964d216707
Cesar Ferracin <cesarferracin@gmail.com>
2022-12-05 10:14:58 -0300

    commit inicial - API OK, iniciando Frontend

---
+++ fe/src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+import { Header } from './components/Header/Header';
+
+export  function App() {
+	return (
+		<>
+			<GlobalStyles />
+			<Header />
+		</>
+	);
+}
